1868d6b5c1986c36342024b172b4269c2f4baccf
mkizka <30231179+mkizka@users.noreply.github.com>
2020-12-17 20:42:37 +0900

    テスト追加

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 example:
 	deno run --allow-net example.ts
+test:
+	deno test --unstable --coverage
+lint:
+	deno fmt && deno lint --unstable
--- src/router_test.ts
+++ src/router_test.ts
@@ -35,13 +35,28 @@ Deno.test("pathToRegExp: 正規表現の記号を含むパスのエスケープ"
   assertEquals(matched?.groups?.path1, "hoge");
 });
 
-Deno.test("Router.(add|match): 正規表現で指定したパスパラメータが返り値に含まれる", () => {
+Deno.test("Router.(add|match): 正規表現でパスパラメータを指定してマッチ", () => {
   const router = new Router();
-  router.add(
-    { path: /hoge\/(?<fuga>.+)/, method: "GET", handler: () => new Response() },
-  );
+  router.add({
+    path: /hoge\/(?<fuga>.+)/,
+    method: "GET",
+    handler: () => new Response(),
+  });
   const [handler, params] = router.match("/hoge/123", "GET");
   assertEquals(params.fuga, "123");
   const response = handler(new Request(), params);
   assertEquals(response.status, 200);
+});
+
+Deno.test("Router.(add|match): マッチしない場合は404が返ってくる", () => {
+  const router = new Router();
+  router.add({
+    path: /hoge\/(?<fuga>.+)/,
+    method: "GET",
+    handler: () => new Response(),
+  });
+  const [handler, params] = router.match("/fuga/123", "GET");
+  assertEquals(params.fuga, undefined);
+  const response = handler(new Request(), params);
+  assertEquals(response.status, 404);
 });
